0efb3d6cab6c24093b1ed09c01c44a1bf30235c4
Konrad <konrad_here@hotmail.com>
2021-03-14 22:53:14 -0600

    Add Item migration / schema

--- migrations/2021-02-24-011200_initial/up.sql
+++ migrations/2021-02-24-011200_initial/up.sql
@@ -16,4 +16,19 @@ SELECT
 CREATE TABLE sessions (
     id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
     created_at timestamp with time zone NOT NULL DEFAULT NOW(),
-    user_id int NOT NULL REFERENCES users (id) ON DELETE CASCADE)
+    user_id int NOT NULL REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE items (
+    id serial PRIMARY KEY,
+    created_at timestamp with time zone NOT NULL DEFAULT NOW(),
+    updated_at timestamp with time zone NOT NULL DEFAULT NOW(),
+    user_id int NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    name text NOT NULL,
+    price int NOT NULL,
+    protection_ends_at timestamp with time zone NOT NULL
+);
+
+SELECT
+    diesel_manage_updated_at ('items');
+
--- src/schema.rs
+++ src/schema.rs
@@ -1,4 +1,16 @@
 table! {
+    items (id) {
+        id -> Int4,
+        created_at -> Timestamptz,
+        updated_at -> Timestamptz,
+        user_id -> Int4,
+        name -> Text,
+        price -> Int4,
+        protection_ends_at -> Timestamptz,
+    }
+}
+
+table! {
     sessions (id) {
         id -> Uuid,
         created_at -> Timestamptz,
@@ -16,9 +28,7 @@ table! {
     }
 }
 
+joinable!(items -> users (user_id));
 joinable!(sessions -> users (user_id));
 
-allow_tables_to_appear_in_same_query!(
-    sessions,
-    users,
-);
+allow_tables_to_appear_in_same_query!(items, sessions, users);
